38dc812565be338be0bba68ce17e73939bb569d7
Walt Beaman <walter.g.beaman@outlook.com>
2023-05-30 05:21:04 -0500

    Train model and test with console input.

--- SentimentAnalyzer/Program.cs
+++ SentimentAnalyzer/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a sentence to evaluate: ");
+            //Load sample data
+            var sampleData = new SentimentAnalysisModel.ModelInput();
+            sampleData.Review = Console.ReadLine();
+
+            //Load model and predict output
+            var result = SentimentAnalysisModel.Predict(sampleData);
+
+            Console.WriteLine($"The sentiment of your sentence is: {result.PredictedLabel}");
+
         }
     }
 }
